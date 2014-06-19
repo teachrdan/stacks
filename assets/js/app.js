4efbd85e429253d034a6d92fe468c0bb0d890c7d
Dan Tennery-Spalding <teachrdan@gmail.com>
2014-06-19 15:08:28 -0700

    Angular local storage module - currently unused

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,35 +6,11 @@ var stacksApp = angular.module('stacksApp', [
 	'ngRoute',
 	'stacksControllers',
 	'stacksServices',
-]);
-
-/*
-stacksApp.config(function($routeProvider, $locationProvider) {
-	
-  $locationProvider.html5Mode(true);
-
-  $routeProvider
-      .when('/', {
-        templateUrl: 'partials/stories_list.html',
-        controller: 'storyController'
-      })
-      .when('/about', {
-        templateUrl: 'partials/about.html',
-        controller: 'aboutController'
-      })
-      .when('/contribute', {
-        templateUrl: 'partials/contribute.html'
-      })
-      .when('/text', {
-        template: 'Test text into index.'
-      })
-      .when('/:storyId', {
-        templateUrl: 'partials/story_detail.html',
-        controller: 'storyDetailController'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-
-  });
-  */
+  'LocalStorageModule'
+])
+.config(['localStorageServiceProvider', function(localStorageServiceProvider){
+  localStorageServiceProvider.setPrefix('stacks');
+  // The following two lines were originally commented out -DTS
+  localStorageServiceProvider.setStorageCookieDomain('stacks.io');
+  localStorageServiceProvider.setStorageType('sessionStorage');
+}]);
